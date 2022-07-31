84025496b434b2c56765815ca7be6d0f9d2e49e2
Peter B. Golbus <pgolbus@gmail.com>
2022-07-31 14:41:08 -0400

    added command line parsing

--- torah_date_classes/utils/vocab.py
+++ torah_date_classes/utils/vocab.py
@@ -1,16 +1,18 @@
-from datetime import timedelta
 import json
+from pathlib import Path
 import random
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
+
+import click
 
 
 ASCII_OFFSET: int = ord('a') - 1
-MOD: int = 7
-RANDOM_SEED: int = 42
-VOCAB_SIZE: int = 8679
-VOCAB_INPUT_FILE: str = 'data/corncob_lowercase.txt'
-OUTPUT_FILE_PATH: str = 'data/{}.json'
 
+
+def get_equivalence_classes(word: str, mod: int) -> Tuple[int, int]:
+    face_value = get_numeric_value(word) % mod
+    hidden_value = get_numeric_value(word, hidden=True) % mod
+    return face_value, hidden_value
 
 def get_numeric_value(word: str, hidden: bool = False) -> int:
     # get_numeric_value('abc') = 6
@@ -22,44 +24,62 @@ def get_numeric_value(word: str, hidden: bool = False) -> int:
         return sum([10**i * ord_offset(char) for i, char in enumerate(word)])
     return sum([ord_offset(char) for char in word])
 
-def get_vocab(vocab_size: int = VOCAB_SIZE, vocab_input_file: str = VOCAB_INPUT_FILE) -> List[str]:
+def get_vocab(vocab_size: int, vocab_input_file: str) -> List[str]:
     with open(vocab_input_file, 'r') as fh:
         full_vocab_list: List[str] = [word.strip() for word in fh.readlines()]
     vocab_list = random.sample(full_vocab_list, vocab_size)
     vocab_list.sort()
     return vocab_list
 
+def write_json(name: str, value: Dict[Any, Any], output_path: str) -> None:
+    path = Path(output_path, f'{name}.json')
+    with open(path, 'w') as fh:
+        json.dump(value, fh, indent=4, sort_keys=True)
 
-if __name__ == '__main__':
-    random.seed(RANDOM_SEED)
-    vocab_list: List[str] = get_vocab()
+@click.command()
+@click.option('-m', '--mod', type=int, default=7, help='Words / mod Words. Defaults to |days of the week|')
+@click.option('--vocab_size',
+    type=int,
+    default=8679,
+    help=('Number of words to sample from a list of English words. Defaults to the number of unique words in the torah')
+    )
+@click.option('--vocab_input_file',
+    type=str,
+    default='data/corncob_lowercase.txt',
+    help=('Path to a file containing a list of (lowercase) words')
+    )
+@click.option('--output_path', type=str, default='data', help='Path to output json blobs')
+@click.option('--seed', type=int, default=42, help='Random seed value')
+def main(mod: int, vocab_size: int, vocab_input_file: str, output_path: str, seed:int) -> None:
+    random.seed(seed)
+
+    vocab_list: List[str] = get_vocab(vocab_size, vocab_input_file)
     vocab: Dict[str, Dict[str, int]] = {}
     face: Dict[int, List[str]] = {}
     hidden: Dict[int, List[str]] = {}
 
     for word in vocab_list:
-        face_value: int = get_numeric_value(word)
-        face_mod: int = face_value % MOD
-        hidden_value: int = get_numeric_value(word, hidden=True)
-        hidden_mod: int = hidden_value % MOD
-        word = word.capitalize()
+        class_values: Tuple[int, int] = get_equivalence_classes(word, mod)
+        face_value:int = class_values[0]
+        hidden_value:int = class_values[1]
         vocab[word]: Dict[str, int] = {}
-        vocab[word]['face']: int = face_mod
-        vocab[word]['hidden']: int = hidden_mod
-        if not face_mod in face:
-            face[face_mod]: List[str] = []
-        face[face_mod].append(word)
-        if not hidden_mod in hidden:
-            hidden[hidden_mod]: List[str] = []
-        hidden[hidden_mod].append(word)
+        vocab[word]['face']: int = face_value
+        vocab[word]['hidden']: int = hidden_value
+        if not face_value in face:
+            face[face_value]: List[str] = []
+        face[face_value].append(word)
+        if not hidden_value in hidden:
+            hidden[hidden_value]: List[str] = []
+        hidden[hidden_value].append(word)
 
     for mod in face.keys():
         face[mod].sort()
         hidden[mod].sort()
 
-    with open(OUTPUT_FILE_PATH.format('face'), 'w') as fh:
-        json.dump(face, fh, indent=4, sort_keys=True)
-    with open(OUTPUT_FILE_PATH.format('hidden'), 'w') as fh:
-        json.dump(hidden, fh, indent=4, sort_keys=True)
-    with open(OUTPUT_FILE_PATH.format('vocab'), 'w') as fh:
-        json.dump(vocab, fh, indent=4, sort_keys=True)
+    write_json('vocab', vocab, output_path)
+    write_json('face', face, output_path)
+    write_json('hidden', hidden, output_path)
+
+
+if __name__ == '__main__':
+    main()
